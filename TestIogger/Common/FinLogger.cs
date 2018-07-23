bd1e03e63e73956dcb5bbbc2ef5e501c034920dd
Finbar Ryan <finbarr@microsoft.com>
2018-07-23 08:42:55 +0100

    Updates

---
+++ TestIogger/Common/FinLogger.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestIogger.Common
+{
+    class FinLogger
+    {
+        public static void Log(string message, LoggerLevel level, LogArguments logArgs)
+        {
+            var loggerMessage = $"Execution: {logArgs.ExecutionId} ({logArgs.FunctionAppName})\n\n{message}";
+
+            //Had to disable logging as we're getting charged a fortune for it by Azure contrary to the way that the Function claims to act.
+
+            if (logArgs.Logger != null)
+                switch (level)
+                {
+                    case LoggerLevel.Info:
+                        logArgs.Logger.LogInformation(loggerMessage);
+                        break;
+                    case LoggerLevel.Success:
+                        logArgs.Logger.LogInformation(loggerMessage);
+                        break;
+                    case LoggerLevel.Error:
+                        logArgs.Logger.LogError(loggerMessage);
+                        break;
+                    case LoggerLevel.Trace:
+                        logArgs.Logger.LogTrace(loggerMessage);
+                        break;
+                }
+        }
+    }
+
+    enum LoggerLevel
+    {
+        Info = 0,
+        Success = 1,
+        Error = 2,
+        Trace = 3,
+    }
+
+    public class LogArguments
+    {
+        public LogArguments(ILogger logger, LoggerApp loggerApp, string executionId, string functionAppName)
+        {
+            Logger = logger;
+            LoggerApp = loggerApp;
+            ExecutionId = executionId;
+            FunctionAppName = functionAppName;
+        }
+
+        public ILogger Logger { get; set; }
+        public LoggerApp LoggerApp { get; set; }
+        public string FunctionAppName { get; set; }
+        public string ExecutionId { get; set; }
+    }
+
+    public enum LoggerApp
+    {
+        ThisApp = 0
+    }
+}
--- TestIogger/Function1.cs
+++ TestIogger/Function1.cs
@@ -6,15 +6,24 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+using TestIogger.Common;
 
 namespace TestIogger
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log, ExecutionContext finCtx)
         {
-            log.Info("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            LogArguments testArg = new LogArguments(log, LoggerApp.ThisApp, finCtx.InvocationId.ToString(), finCtx.FunctionName.ToString());
+
+            FinLogger.Log("Test Error", LoggerLevel.Error, testArg);
+            FinLogger.Log("Test Information ", LoggerLevel.Info, testArg);
+            FinLogger.Log("Test Success", LoggerLevel.Success, testArg);
+            FinLogger.Log("Test Trace", LoggerLevel.Trace, testArg);
 
             string name = req.Query["name"];
 
